501d67bb62cfec23a81a33590219b7ea6c10976c
mayo7e <mayowa.wh@gmail.com>
2022-11-28 08:28:42 +0000

    Update - style for modal

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ import Menu from "./assets/hamburger.svg"
 import Close from "./assets/close.png"
 import Bookmark from  "./assets/bookmark.png"
 import RangeSlider from  "./assets/rangeslider.png"
-// import Backproject from "./backproject.js"
+import Backproject from "./backproject.js"
 
 
 
@@ -135,64 +135,7 @@ const Home = () => {
 
             
         </main>
-        <div id="overlay-02">
-    <div className='backproject' >
-        <section>
-            <div>
-                <h2>Back this project</h2>
-                <img className="close-icon" alt="close icon" src="0#" />
-            </div>
-            <p>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world? </p>
-        </section>
-
-        <section>
-            <div>
-                <input  type="checkbox" />
-                <div>
-                    <h3>Pledge with no reward</h3>
-                </div>
-            </div>
-            <p>Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.</p>
-        </section>
-
-        <section>
-            <div>
-                <input  type="checkbox" />
-                <div>
-                    <h3>Bamboo Stand</h3>
-                    <h3>Pledge $25 or more</h3>
-                </div>
-            </div>
-            <p>You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.</p>
-            <p>101 <span>left</span></p>
-        </section>
-
-        <section>
-            <div>
-                <input  type="checkbox" />
-                <div>
-                    <h3>Black Edition Stand</h3>
-                    <h3>Pledge $25 or more</h3>
-                </div>
-            </div>
-            <p>You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included..</p>
-            <p>64 <span>left</span></p>
-        </section>
-
-        <section>
-            <div>
-                <input  type="checkbox" />
-                <div>
-                    <h3>Mahogany Special Edition</h3>
-                    <h3>Pledge $25 or more</h3>
-                </div>
-            </div>
-            <p>You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.</p>
-            <p>0<span>left</span></p>
-        </section>
-
-        </div>
-    </div>
+       <Backproject />
 
                         
     </div>
