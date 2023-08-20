af4ae0fca2fb732fe8220c8bd40e6823ce43f6c4
mayo7e <mayowa.wh@gmail.com>
2023-08-20 17:34:06 +0100

    Add css. custom properties for typograpghy and color

--- src/container/App.js
+++ src/container/App.js
@@ -1,5 +1,5 @@
 
-import Home from "./Home/home.js"
+import Home from "./Home"
 
 
 
---
+++ src/container/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return ( 
+        <h1 className="heading-lg-semibold">Hello World</h1>
+    );
+}
+ 
+export default Home;
